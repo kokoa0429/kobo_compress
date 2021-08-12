666e64cc7968f08a8ed6f9356dee72a63cbc99e6
kokoa0429 <kokoa@ciebus.net>
2021-08-12 16:34:12 +0900

    [server] feat: docker

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -11,7 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'test',
+      database: 'root',
       entities: [],
       synchronize: true,
     }),
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -7,7 +7,13 @@ export class AppService {
   }
 
   getPost(): any {
-
+    return {
+      posts: [
+        {
+          title: 'hogehoge',
+          img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAIAAAD8GO2jAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABlSURBVEhL7c1RDoAwCANQjsH9L6pk1MSogQ3ZX9/XumkrNaqKE1HJcUHugtbBo9838FKDPDxikfca5JvPywXea5BfgqdcXG3SDyIzP+9tN78GcArVByZxIMWBFAdSHEhtH6A2IicT+YbCK6P1hAAAAABJRU5ErkJggg==',
+        },
+      ],
+    };
   }
-
 }
